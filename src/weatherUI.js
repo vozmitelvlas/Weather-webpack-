28a1bff364fdf81f1980eee36bdc63cd467eeba6
vozmitelvlas <vozmitel.vlas228@mail.ru>
2025-11-09 15:35:48 +0300

    feat(weatherUI): change bg via widget click

---
+++ src/weatherUI.js
@@ -0,0 +1,28 @@
+export default class WeatherUI {
+    constructor(audioController) {
+        this.audioController = audioController;
+        this.widgets = document.querySelectorAll('.widget');
+        this.weatherType = null;
+        this.bindEvents();
+    };
+
+    bindEvents() {
+        this.widgets.forEach((widget) =>
+            widget.addEventListener('click', () =>
+                this.handleClick(widget)
+            )
+        );
+    };
+
+    handleClick(weatherWidget) {
+        document.querySelector(`[data-weather="${this.weatherType}"]`)?.classList.remove('clicked');
+        this.weatherType = weatherWidget.dataset.weather;
+        document.body.className = `${this.weatherType}-bg`;
+        weatherWidget.classList.add('clicked');
+        // this.audioController.play(this.weatherType);
+    };
+
+    handleVolumeChange(/*value*/) {
+        //this.audioController.setVolume(value);
+    };
+};
